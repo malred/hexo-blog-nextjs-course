4a44ef2267722b149a85ba804ac654179dc3e0c1
malred <13695024677@163.com>
2024-08-25 21:28:21 +0800

    layout

--- components/footbar.tsx
+++ components/footbar.tsx
@@ -1,6 +1,8 @@
+// components/footbar.tsx
+
 export default function Footbar() {
     return (
-        <footer className={'border-b border-b-gray-300 border-t border-t-gray-300 sticky h-20 w-full'}>
+        <footer className={'bg-gray-200 border-b border-b-gray-300 border-t border-t-gray-300 sticky h-20 w-full'}>
             footbar
         </footer>
     )
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
+// components/navbar.tsx
+
 export default function Navbar() {
     return (
-        <nav className={'border-b border-b-gray-300 sticky w-full h-20'}>
+        <nav className={'bg-green-200 border-b border-b-gray-300 sticky w-full h-20'}>
             navbar
         </nav>
     )
